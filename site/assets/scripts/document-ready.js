755ccec24fda333212a3ccd6bb6e69ddcdcd5ae3
Jason Law <jlaw@sapient.com>
2014-09-23 14:52:02 -0400

    added screens for sections

--- site/assets/scripts/document-ready.js
+++ site/assets/scripts/document-ready.js
@@ -1,6 +1,6 @@
 "use strict";
 
-!function() {
+var directory = (function () {
 
     var w = window
         , d = document
@@ -11,80 +11,41 @@
         , landingContainer = '.landing'
         ;
 
+    return {
 
-    // Fade-Out Sapient Landing
-    setTimeout(function(){ $(landingContainer).fadeOut(200) }, 2000);
+        appInit: function(){
+            // Fade-Out Sapient Landing
+            setTimeout(function(){ $(landingContainer).fadeOut(200) }, 2000);
+            // Initialize Parse Application
+            Parse.initialize("uNtjzJdbGtEmC5n6ZoB3MkYbdlB23i5qeXejOT0O", "N1wO2Ceogq6ZPld1F6J6N4I6q6P4K8UXgWmI1yyu");
+            this.events();
+        },
 
-    // Initialize Parse Application
-    Parse.initialize("uNtjzJdbGtEmC5n6ZoB3MkYbdlB23i5qeXejOT0O", "N1wO2Ceogq6ZPld1F6J6N4I6q6P4K8UXgWmI1yyu");
+        events: function(){
 
-    [].forEach.call(document.querySelectorAll('nav a'), function(el) {
-        el.addEventListener('click', function(e) {
-            e.preventDefault();
-            var that = this;
-            var section = '.' + that.getAttribute('data-section');
-            var allSections = '.section';
+            // Nav Menu Event Handler
+            [].forEach.call(document.querySelectorAll('nav a'), function(el) {
+                el.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    var that = this;
+                    var section = '.' + that.getAttribute('data-section');
+                    var allSections = '.section';
 
-            $('.section').fadeOut(200).promise().done(function(){
-                $(section).fadeIn(200);
-            })
+                    $('.section').fadeOut(200).promise().done(function(){
+                        $(section).fadeIn(200);
+                    })
 
-        });
-    });
-
-
-    // settings nav icon click
-    /*
-    $(document).on("click",".settings-nav-icon",function() {
-        $this = $(this);
-        if(!($this).hasClass("open")) {
-            $('.side-panel').animate({ width: "0%", opacity: 0 }, 'slow', function() {
-                $('.side-panel').removeClass("open");
-                $('.side-panel').html("");
-            });
-            $('.settings-menu').animate({ width: "25%", opacity: 1 }, 'slow', function() {});
-            $('.content-wrapper').animate({ width: "75%" }, 'slow', function() {});
-            $(this).addClass("open");
-            $(".settings-nav-icon span").removeClass("icon-menu2").html("X");
-        } else {
-            $(".settings-nav-icon span").addClass("icon-menu2").html("");
-            $(this).removeClass("open");
-            $('.content-wrapper').animate({ width: "100%" }, 'slow', function() {});
-            $('.settings-menu').animate({ width: "0%", opacity: 0 }, 'slow', function() {});
-        }
-    });
-
-    $(document).on("click",".tool",function() {
-        $this = $(this);
-        var tool = ($(this).attr('class').split(' ')[1]);
-        $(".settings-nav-icon span").addClass("icon-menu2").html("");
-        $(".settings-nav-icon").removeClass("open");
-        $('.content-wrapper').animate({ width: "100%" }, 'slow', function() {});
-        $('.settings-menu').animate({ width: "0%", opacity: 0 }, 'slow', function() {});
-
-        if($('.side-panel').hasClass("open")) {
-            $('.side-panel').animate({ width: "0%", opacity: 0 }, 'fast', function() {
-                $('.side-panel').animate({ width: "80%", opacity: 1 }, 'fast', function() {
-                    $('.side-panel').addClass("open");
                 });
-                $('.side-panel').load('includes/'+tool+'.html');
             });
-        } else {
-            $('.side-panel').animate({ width: "80%", opacity: 1 }, 'fast', function() {
-                $('.side-panel').addClass("open");
-                $('.side-panel').load('includes/'+tool+'.html');
-            });
+
         }
-    });
 
-    $(document).on("click",".close-sidepanel",function() {
-        $('.side-panel').animate({ width: "0%", opacity: 0 }, 'slow', function() {
-            $('.side-panel').removeClass("open");
-            $('.side-panel').html("");
-        });
-    });
-*/
+    }
+
+})();
+
+directory.appInit();
 
 
-}();
+
 
