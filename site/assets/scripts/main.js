893f9d9199bee128130c671dce980b3484ff4e6b
Jason Law <jlaw@sapient.com>
2014-09-23 12:59:04 -0400

    added parse app initialization

--- site/assets/scripts/main.js
+++ site/assets/scripts/main.js
@@ -9,16 +9,22 @@ function fade(e,t,n,r){function a(){s=i?s+u:s-u;n.style.opacity=s;if(r){n.style.
 
 !function() {
 
-    var w = window,
-        d = document,
-        e = d.documentElement,
-        g = d.getElementsByTagName('body')[0],
-        winWidth = w.innerWidth || e.clientWidth || g.clientWidth,
-        winHeight = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    var w = window
+        , d = document
+        , e = d.documentElement
+        , g = d.getElementsByTagName('body')[0]
+        , winWidth = w.innerWidth || e.clientWidth || g.clientWidth
+        , winHeight = w.innerHeight|| e.clientHeight|| g.clientHeight
+        , landingContainer = document.querySelector('.landing')
+        ;
 
-    var landingContainer = document.querySelector('.landing');
-
+    // Fade-Out Sapient Landing
     setTimeout(function(){ fade('out', 500, landingContainer, true) }, 2000);
+
+    // Initialize Parse Application
+    Parse.initialize("uNtjzJdbGtEmC5n6ZoB3MkYbdlB23i5qeXejOT0O", "N1wO2Ceogq6ZPld1F6J6N4I6q6P4K8UXgWmI1yyu");
+
+
 
     // settings nav icon click
     /*
@@ -75,13 +81,6 @@ function fade(e,t,n,r){function a(){s=i?s+u:s-u;n.style.opacity=s;if(r){n.style.
 
 }();
 
-
-(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-e.src='//www.google-analytics.com/analytics.js';
-r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-ga('create','UA-XXXXX-X');ga('send','pageview');
 
 (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
 function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
