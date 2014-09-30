1256a67252ea42a023e112272f09b1ec8c31aec5
Jason Law <jlaw@sapient.com>
2014-09-30 16:38:52 -0400

    added new parse table

--- dev/assets/scripts/document-ready.js
+++ dev/assets/scripts/document-ready.js
@@ -23,7 +23,7 @@ var directory = (function () {
         },
 
         queryData: function(){
-            var UserData = Parse.Object.extend("directory");
+            var UserData = Parse.Object.extend("directory_data");
             var query = new Parse.Query(UserData);
             var that = this;
             query.find({
